0041e1c06693bd7180e50a1fead9b0bbbdcbf5da
manahari <68924218+sushant1913@users.noreply.github.com>
2022-01-11 23:50:06 +0545

    Add files via upload

---
+++ dhasu/resp.js
@@ -0,0 +1,18 @@
+burger = document.querySelector(".burger")
+burger = document.querySelector(".navbar")
+burger = document.querySelector(".navlist")
+
+
+
+
+burger.addEventListener("click",()=>{
+  navlist.classList.toggle('v-class-resp');
+  navbar.classList.toggle('h-nav-resp');
+  
+
+ 
+
+
+
+
+})
